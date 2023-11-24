ea029428d1780ff297d0afb9134c06b1338022d5
Muhammad Fazeel <126400735+Fazeel355@users.noreply.github.com>
2023-11-24 18:50:48 +0500

    my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -9,42 +9,40 @@ classifier = pipeline('sentiment-analysis')
 def main():
     st.title("Text Classification App")
 
-    # Option to either input text or upload a file
-    option = st.radio("Choose input method:", ["Enter Text", "Upload File"])
+    # File upload
+    uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
-    if option == "Enter Text":
-        # Input text area
-        text_input = st.text_area("Enter your text here:", "Type your text here...")
-        if st.button("Get Classification"):
-            # Make prediction
-            result = classifier(text_input)
-
-            # Display result
-            st.write("Prediction:")
-            st.write(f"Label: {result[0]['label']}")
-            st.write(f"Score: {result[0]['score']:.4f}")
-    else:
-        # File upload
-        uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-
-        if uploaded_file is not None:
-            # Load the dataset
-            df = pd.read_csv(uploaded_file)
-
-            # Check if 'text' column exists
-            if 'text' not in df.columns:
-                st.error("The 'text' column is missing in the dataset.")
+    if uploaded_file is not None:
+        try:
+            # Determine file type and load the dataset
+            if uploaded_file.name.endswith('.csv'):
+                df = pd.read_csv(uploaded_file)
+            elif uploaded_file.name.endswith(('.xls', '.xlsx')):
+                df = pd.read_excel(uploaded_file, engine='openpyxl')
             else:
-                # Make predictions for each text in the dataset
-                predictions = [classifier(text) for text in df['text']]
+                st.error("Unsupported file type. Please upload a CSV or Excel file.")
+                st.stop()
+        except pd.errors.EmptyDataError:
+            st.error("The uploaded file is empty.")
+            st.stop()
+        except pd.errors.ParserError as e:
+            st.error(f"Error parsing the file: {e}")
+            st.stop()
 
-                # Display overall results
-                st.write("Overall Results:")
-                for i, prediction in enumerate(predictions):
-                    st.write(f"Row {i + 1}:")
-                    st.write(f"Label: {prediction[0]['label']}")
-                    st.write(f"Score: {prediction[0]['score']:.4f}")
-                    st.write("---")
+        # Check if 'text' column exists
+        if 'text' not in df.columns:
+            st.error("The 'text' column is missing in the dataset.")
+        else:
+            # Make predictions for each text in the dataset
+            predictions = [classifier(text) for text in df['text']]
+
+            # Display overall results
+            st.write("Overall Results:")
+            for i, prediction in enumerate(predictions):
+                st.write(f"Row {i + 1}:")
+                st.write(f"Label: {prediction[0]['label']}")
+                st.write(f"Score: {prediction[0]['score']:.4f}")
+                st.write("---")
 
 if __name__ == "__main__":
     main()
